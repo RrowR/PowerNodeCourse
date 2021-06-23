55c1c05672661a1bd92c64df0d0a4b4ddde4b0ff
Memo <2415782224@qq.com>
2021-06-23 18:37:51 +0800

    隐式类型转换

---
+++ JavaSE/src/test/java/com/test/day02/Demo03HiddenType.java
@@ -0,0 +1,12 @@
+package com.test.day02;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo03HiddenType {
+    @Test
+    public void testCharToInt(){
+        //将char类型转换为int类型(隐式类型转换)
+        int a = 'A';
+        System.out.println(a);
+    }
+}
