d4c84304a52b87e6f22425a94cc1c4ea83409743
Memo <2415782224@qq.com>
2021-06-24 10:23:30 +0800

    变量放右边，常量放左边，即使==写成了=，就能将程序的报错级别从运行期间体检到编译期间，这其实就是代码规范

--- JavaSE/src/test/java/com/test/day03/Demo01.java
+++ JavaSE/src/test/java/com/test/day03/Demo01.java
@@ -5,6 +5,20 @@ import org.junit.jupiter.api.Test;
 public class Demo01 {
 
     @Test
+    public void test05(){
+        /**
+         * 比较运算符
+         */
+        boolean b = false;
+        // 变量放右边，常量放左边，即使==写成了=，就能将程序的报错级别从运行期间体检到编译期间，这其实就是代码规范
+        if (true == b){
+            System.out.println("答案为相等");
+        }else {
+            System.out.println("答案不等");
+        }
+    }
+
+    @Test
     /**
      * 扩展赋值运算符:
      *      不仅把右边看作一个整体，并且还会将右边整体的数据强转成左边数据的类型
