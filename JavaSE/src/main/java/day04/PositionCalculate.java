3820e3da54ecf2ee1c7ec9739f941292e478e4e9
Memo <2415782224@qq.com>
2021-06-25 16:05:45 +0800

    三元运算符的使用以及位运算符的使用以及使用^运算符实现2数交换

--- JavaSE/src/main/java/day04/PositionCalculate.java
+++ JavaSE/src/main/java/day04/PositionCalculate.java
@@ -4,17 +4,83 @@ package day04;
  * 位运算：
  *      >> 右移   对m右移n位，则/^n,负数除外
  *      << 左移   对m左移n位，则*^n
+ *      >>> 无符号右移   无论是整数还是负数，高位永远补0
  *      &   与   如果是数值型，则是对二进制运算，如果是boolean类型，则是做并且运算
  *      |   或   如果是数值型，则是对二进制运算，如果是boolean类型，则是做并或运算
  *      ^   异或  如果是数值型，则是对二进制运算，如果是boolean类型，则是做异或运算
- *      >>> 无符号右移   无论是整数还是负数，高位永远补0
- *
+ *      ~ 非位运算
  */
 public class PositionCalculate {
     public static void main(String[] args) {
 //        rightMove();
 //        leftMove();
-        noSymbolMove();
+//        noSymbolMove();
+//        AndMove();
+//        OrMove();
+//        DiffOr();
+//        NoPositionCalculate();
+        ExchangeTwoNumbers();
+    }
+
+    private static void ExchangeTwoNumbers() {
+        /**
+         *
+         * 使用异或运算来交换2个数
+         */
+        int a = 3,b = 5;
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a="+a+",b="+b);
+    }
+
+    private static void NoPositionCalculate() {
+        /**
+         * 二进制位取反
+         *      0001
+         *      1110     //取反
+         *      0001     //负数取反
+         * 100..0010     //+1
+         */
+        int a = ~1;
+        System.out.println(a);
+    }
+
+    private static void DiffOr() {
+        /**
+         * 不同位1
+         * 0011
+         * 0101
+         * 0110
+         */
+        int a = 3 ^ 5;
+        System.out.println(a);
+    }
+
+    private static void OrMove() {
+        /**
+         * 只要有一个是1就是1
+         * 0010
+         * 0101
+         * 0111
+         */
+        int a = 2;
+        int b = 5;
+        int c = a | b;
+        System.out.println(c);
+    }
+
+    private static void AndMove() {
+        //与运算
+        /**
+         *      0011
+         *      0101
+         *      0001
+         */
+        int a = 3;
+        int b = 5;
+        int c = a & b;
+        System.out.println(c);
     }
 
     private static void noSymbolMove() {
