6c2ad7565c8ba9fa4632ba038be20070d5a2d049
Memo <2415782224@qq.com>
2021-06-26 19:16:29 +0800

    流程控制1~7

--- JavaSE/src/test/java/com/test/day05/DemoTest01.java
+++ JavaSE/src/test/java/com/test/day05/DemoTest01.java
@@ -93,4 +93,54 @@ public class DemoTest01 {
             }
         }
     }
+
+    @Test
+    public void test06(){
+        //求100到999之间的水仙花数。水仙花数的每个位上的数字的3次幂之和等于它本身（例如：1^3 + 5^3+ 3^3 = 153）
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入一个100~999之间的数");
+        int number = input.nextInt();
+        if (number < 100 || number > 999){
+            System.out.println("您输入的值不在范围内");
+        }else {
+            int i = number;
+            for (int j = 100; j <= i; j++) {
+                //个位数
+                int k = j % 10;
+                //十位数
+                int o = j / 10 % 10;
+                //百位数
+                int p = j / 100;
+                if (j == k*k*k + o*o*o + p*p*p){
+                    System.out.println("水仙花数为："+j);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void test07(){
+        //编程找出四位整数abcd中满足下述关系的数，(ab+cd)*(ab+cd)=abcd（例如：(20 + 25) * (20 + 25)= 2025）。
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入一个1000~9999之间的数");
+        int number = input.nextInt();
+        if (number < 1000 || number > 9999){
+            System.out.println("您输入的数不符合要求！！！");
+        }else {
+            int i = number;
+            for (int j = 1000; j < i; j++) {
+                //求个位数
+                int b = j % 10;
+                //求十位数
+                int c = j / 10 % 10;
+                //求百位数
+                int d = j / 100 % 10;
+                //求千位数
+                int e = j / 1000 % 10;
+                if (j == ((e*10 + d)+(c*10 + b))*((e*10 + d)+(c*10 + b))){
+                    System.out.println("满足要求的数有："+j);
+                }
+            }
+        }
+    }
 }
---
+++ JavaSE/src/test/java/com/test/day05/SelfTest.java
@@ -0,0 +1,19 @@
+package com.test.day05;
+
+public class SelfTest {
+    public static void main(String[] args) {
+        int a = 9876;
+        //求个位数
+        int b = a % 10;
+        System.out.println(b);
+        //求十位数
+        int c = a / 10 % 10;
+        System.out.println(c);
+        //求百位数
+        int d = a / 100 % 10;
+        System.out.println(d);
+        //求千位数
+        int e = a / 1000 % 10;
+        System.out.println(e);
+    }
+}
