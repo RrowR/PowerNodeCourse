8a83cfbf29ddf876e59b0d04674231d953a43695
Memo <2415782224@qq.com>
2021-07-02 15:03:17 +0800

    课堂测试2

--- JavaSE/src/test/java/com/test/day09/ClassExaminations.java
+++ JavaSE/src/test/java/com/test/day09/ClassExaminations.java
@@ -23,45 +23,63 @@ public class ClassExaminations {
             switch (i) {
                 case 1:
                     System.out.println("您的第" + i + "年本金为" + money + "利息为:" + money * 0.0225);
-                break;
+                    break;
                 case 2:
                     System.out.println("您的第" + i + "年本金为" + (money * 0.0270 + money) + "利息为:" + ((money * 0.0270 + money) * 0.027));
-                break;
+                    break;
                 case 3:
-                    System.out.println("您的第" + i + "年本金为" + ((money * 0.0324 + money) + ((money * 0.0324 + money) * 0.0324))  + "利息为:" + (0.0324 * ((money * 0.0324 + money) + ((money * 0.0324 + money) * 0.0324))));
-                break;
+                    System.out.println("您的第" + i + "年本金为" + ((money * 0.0324 + money) + ((money * 0.0324 + money) * 0.0324)) + "利息为:" + (0.0324 * ((money * 0.0324 + money) + ((money * 0.0324 + money) * 0.0324))));
+                    break;
                 case 4:
-                    System.out.println("您的第" + i + "年本金为" + (((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360))+(0.0360 * ((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360)))) + "利息为:"+(0.0360 * (((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360))+(0.0360 * ((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360))))));
-                break;
+                    System.out.println("您的第" + i + "年本金为" + (((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360)) + (0.0360 * ((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360)))) + "利息为:" + (0.0360 * (((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360)) + (0.0360 * ((money * 0.0360 + money) + ((money * 0.0360 + money) * 0.0360))))));
+                    break;
             }
         }
     }
 
     @Test
-    public void thirdAnswer(){
+    public void thirdAnswer() {
         Scanner input = new Scanner(System.in);
         System.out.println("请输入四位整数");
         int number = input.nextInt();
         int sum = 0;
         int[] arr = new int[4];
-        arr[3] = (int)(number / 1 % 10);        // 第一位
-        arr[2] = (int)(number / 10 % 10);       // 第二位
-        arr[1] = (int)(number / 100 % 10);      // 第三位
-        arr[0] = (int)(number / 1000 % 10);     // 第四位
-        for (int i = 0; i < 4 ; i++) {
+        arr[3] = (int) (number / 1 % 10);        // 第一位
+        arr[2] = (int) (number / 10 % 10);       // 第二位
+        arr[1] = (int) (number / 100 % 10);      // 第三位
+        arr[0] = (int) (number / 1000 % 10);     // 第四位
+        for (int i = 0; i < 4; i++) {
             sum += arr[i];
         }
         System.out.println(sum);
     }
 
     @Test
-    public void thirdAnswer2(){
+    public void thirdAnswer2() {
         Scanner input = new Scanner(System.in);
         System.out.println("请输入一个整数");
         int number = input.nextInt();
         int sum = 0;
-        for (int i = 1; (number / i) > 0 ; i *= 10) {
-            sum += (int)(number / i % 10);
+        for (int i = 1; (number / i) > 0; i *= 10) {
+            sum += (int) (number / i % 10);
+        }
+        System.out.println(sum);
+    }
+
+    @Test
+    public void thirdAnswer3() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入一个整数");
+        int number = input.nextInt();
+        int sum = 0;
+        while (true){
+            int bit = number % 10;
+            System.out.println(bit);
+            number /= 10;
+            sum += bit;
+            if (number == 0){
+                break;
+            }
         }
         System.out.println(sum);
     }
