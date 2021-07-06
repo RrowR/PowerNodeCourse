3180c2f4d41a3dad09479ddf4ab921b25ea110a0
Memo <2415782224@qq.com>
2021-07-06 11:55:26 +0800

    使用假设法优化冒泡排序

--- JavaSE/src/main/java/day12/BubbleSort.java
+++ JavaSE/src/main/java/day12/BubbleSort.java
@@ -7,14 +7,20 @@ public class BubbleSort {
         int[] arr = {2,4,1,5,7,9,13,8};
         // 比较的次数 = 索引的次数
         for (int i = 0; i < arr.length - 1; i++) {
+            boolean flag = true;
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j+1]){
                     arr[j] = arr[j] + arr[j+1];
                     arr[j+1] = arr[j] - arr[j+1];
                     arr[j] = arr[j] - arr[j+1];
+                    flag = false;
                 }
             }
+            if (flag == true){
+                break;
+            }
+            System.out.print("第"+(i+1)+"趟排序");
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println(Arrays.toString(arr));
     }
 }
