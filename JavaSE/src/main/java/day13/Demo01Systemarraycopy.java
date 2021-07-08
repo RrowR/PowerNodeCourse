6d80449f3c229ea22866777dd54ecd0d2eee7580
Memo <2415782224@qq.com>
2021-07-08 11:51:05 +0800

    java.lang 包下的System.arraycopy方法

--- JavaSE/src/main/java/day13/Demo01Systemarraycopy.java
+++ JavaSE/src/main/java/day13/Demo01Systemarraycopy.java
@@ -10,6 +10,25 @@ import java.util.Arrays;
  */
 public class Demo01Systemarraycopy {
     public static void main(String[] args) {
+//        copyPosition();
+        moveBackElement();
+        moveForwardElement();
+    }
+
+    private static void moveForwardElement() {
+        int[] arr = {11,22,33,44,55,66,77};
+//        System.arraycopy();
+    }
+
+    private static void moveBackElement() {
+        int[] arr = {11,22,33,44,55,66,77};
+        int num = 6;
+        System.arraycopy(arr,0,arr,1,num);
+        arr[0] = 0;
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void copyPosition() {
         int[] arr = {1,2,3,4,5};
         int[] arr2 = new int[arr.length];
         System.arraycopy(arr,2,arr2,2,3);
