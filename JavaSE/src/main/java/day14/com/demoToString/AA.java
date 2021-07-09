79e35034485c8c5e95a7d7a6abec060337ee1de2
Memo <2415782224@qq.com>
2021-07-09 10:17:48 +0800

    字符类toString快捷键的使用，注意子类在重写toString的时候是有选项的

---
+++ JavaSE/src/main/java/day14/com/demoToString/AA.java
@@ -0,0 +1,14 @@
+package day14.com.demoToString;
+
+public class AA {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "AA{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
