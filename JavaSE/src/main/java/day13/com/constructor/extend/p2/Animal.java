4798d2bf6f9c3729620454188a215906f938a75f
MeroMemo <1043526080@qq.com>
2021-07-10 00:44:34 +0800

    继承构造方法的执行顺序˚

---
+++ JavaSE/src/main/java/day13/com/constructor/extend/p2/Animal.java
@@ -0,0 +1,12 @@
+package day13.com.constructor.extend.p2;
+
+public class Animal {
+    private String color;
+
+    public Animal(String color) {
+        this.color = color;
+    }
+
+    public Animal() {
+    }
+}
