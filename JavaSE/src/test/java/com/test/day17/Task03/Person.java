e02b575c0dd4f1bd396bb6aadf6bd15b63ca75e9
Memo <2415782224@qq.com>
2021-07-13 21:33:54 +0800

    将对象进行排序

---
+++ JavaSE/src/test/java/com/test/day17/Task03/Person.java
@@ -0,0 +1,52 @@
+package com.test.day17.Task03;
+
+public class Person implements Comparable{
+    public String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (age > ((Person) o).age){
+            return 1;
+        }else if (age < ((Person) o).age){
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+
+    public void sortArr(Object arr[]){
+        Object o = null;
+        // 排序前
+        for (Object o1 : arr) {
+            String name = ((Person) o1).name;
+            int age = ((Person) o1).age;
+            System.out.println("name="+name+",age="+age);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                // 这里面直接调用就好了，方法比比较的就是这两个对象的age属性,当为-1的时候，交换2个数，结果将是一个
+                if (((Person)arr[i]).compareTo(((Person)arr[j])) == -1){
+                    o = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = o;
+                }
+            }
+        }
+        System.out.println("=================分割线======================");
+        // 排序后
+        for (Object o1 : arr) {
+            String name = ((Person) o1).name;
+            int age = ((Person) o1).age;
+            System.out.println("name="+name+",age="+age);
+        }
+    }
+}
---
+++ JavaSE/src/test/java/com/test/day17/Task03/TestMain.java
@@ -0,0 +1,14 @@
+package com.test.day17.Task03;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Person p1 = new Person("美美", 9);
+        Person p2 = new Person("猫羽雫", 16);
+        Person p3 = new Person("镜华", 11);
+        Person[] people = new Person[3];
+        people[0] = p1;
+        people[1] = p2;
+        people[2] = p3;
+        new Person().sortArr(people);
+    }
+}
