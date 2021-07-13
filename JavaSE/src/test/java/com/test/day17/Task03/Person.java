064ad76df6686c4b6986254b5677fc5751ade1db
MeroMemo <1043526080@qq.com>
2021-07-13 22:53:50 +0800

    实现compareable接口，实现对象的排序

--- JavaSE/src/test/java/com/test/day17/Task03/Person.java
+++ JavaSE/src/test/java/com/test/day17/Task03/Person.java
@@ -32,8 +32,10 @@ public class Person implements Comparable{
             System.out.println("name="+name+",age="+age);
         }
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 // 这里面直接调用就好了，方法比比较的就是这两个对象的age属性,当为-1的时候，交换2个数，结果将是一个
+                // 如果是这样的冒泡排序，那么就是倒序
+                // 如果 j 是从 0 开始，那么就是普通的顺序
                 if (((Person)arr[i]).compareTo(((Person)arr[j])) == -1){
                     o = arr[i];
                     arr[i] = arr[j];
