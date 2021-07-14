ca94e461817033dc9a5ac90423e6f6a29a52d1f4
Memo <2415782224@qq.com>
2021-07-14 21:00:56 +0800

    oop

---
+++ JavaSE/src/test/java/com/test/day17/oop/Mobile1.java
@@ -0,0 +1,6 @@
+package com.test.day17.oop;
+
+public abstract class Mobile1 {
+    public void call(){}
+    public void sendMsg(){}
+}
---
+++ JavaSE/src/test/java/com/test/day17/oop/Mobile4.java
@@ -0,0 +1,25 @@
+package com.test.day17.oop;
+
+public class Mobile4 extends Mobile3{
+    public void surf(){}
+
+    @Override
+    public void call() {
+        super.call();
+    }
+
+    @Override
+    public void sendMsg() {
+        super.sendMsg();
+    }
+
+    @Override
+    public void photo() {
+        super.photo();
+    }
+
+    @Override
+    public void listenMusic() {
+        super.listenMusic();
+    }
+}
