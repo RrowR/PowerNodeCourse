608ecbc78bc5f2e76917c72f2195b487999cd35b
Memo <2415782224@qq.com>
2021-07-15 18:57:43 +0800

    Calendar类是一个抽象方法，通过调用getInstance来获得Calendar的子类对象

---
+++ JavaSE/src/test/java/com/test/day18/Test03.java
@@ -0,0 +1,10 @@
+package com.test.day18;
+
+import java.util.Calendar;
+
+public class Test03 {
+    public static void main(String[] args) {
+        Calendar instance = Calendar.getInstance();
+        System.out.println(instance);
+    }
+}
