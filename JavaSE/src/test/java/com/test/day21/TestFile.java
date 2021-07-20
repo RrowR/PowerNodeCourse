f65043178ccd22a15f081a994f11657898353ddf
Memo <2415782224@qq.com>
2021-07-20 16:03:30 +0800

    当写入数据到指定文件中的时候，路径如果不存在还是会报错

--- JavaSE/src/test/java/com/test/day21/TestFile.java
+++ JavaSE/src/test/java/com/test/day21/TestFile.java
@@ -9,8 +9,9 @@ import java.io.IOException;
 public class TestFile {
     @Test
     public void Test01() throws IOException {
-        File file = new File("src\\test\\java\\com\\test\\day21\\a.txt");
+        File file = new File("src\\test\\java\\com\\test\\day21\\b\\a.txt");
         FileOutputStream fos = new FileOutputStream(file);
+        // src\test\java\com\test\day21\b\a.txt (系统找不到指定的路径。) 如果指定文件路径的文件夹路劲不存在还是会报错
         fos.write("猫羽雫".getBytes());
         fos.close();
     }
