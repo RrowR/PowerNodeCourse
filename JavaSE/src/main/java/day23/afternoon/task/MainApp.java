356ccf09ec94347aa23e00774bcdb0712dbd9737
Memo <2415782224@qq.com>
2021-07-23 22:01:11 +0800

    自动生成代码

---
+++ JavaSE/src/main/java/day23/afternoon/task/MainApp.java
@@ -0,0 +1,9 @@
+package day23.afternoon.task;
+
+import java.io.FileNotFoundException;
+
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+        GenerateCode.generate();
+    }
+}
