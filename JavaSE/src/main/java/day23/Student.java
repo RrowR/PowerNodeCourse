81d11ec5c3c8705cba4f4ada2cd390a7fcca0e21
MeroMemo <1043526080@qq.com>
2021-07-24 00:09:00 +0800

    根据entity自动生成添加了Lombok的代码

---
+++ JavaSE/src/main/java/day23/Student.java
@@ -0,0 +1,16 @@
+package day23;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String userName;
+    private Integer userAge;
+    private Date birthDay;
+}
