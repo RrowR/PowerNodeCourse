87d20b01a541e07def9cff289111ff82e4846c4f
Memo <2415782224@qq.com>
2021-07-24 08:14:54 +0800

    updateFile

--- JavaSE/src/main/java/day23/Student.java
+++ JavaSE/src/main/java/day23/Student.java
@@ -1,14 +1,9 @@
 package day23;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Student {
     private String userName;
     private Integer userAge;
