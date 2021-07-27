dfe06617341ffe13f46b39447155bc6999d92cb3
Memo <2415782224@qq.com>
2021-07-27 09:04:19 +0800

    使用Excutors来创建线程池

--- JavaSE/src/test/java/com/test/day25/Task03/MainApp.java
+++ JavaSE/src/test/java/com/test/day25/Task03/MainApp.java
@@ -10,6 +10,12 @@ public class MainApp {
         /*
             实现Callable接口的类想要实现多线程只能让线程池来调用，使用get方法来获取这个线程的返回值
             拿到结果之后注意要关闭线程池，否则线程池将一直等待接收要执行的实现Callable接口里的run方法的那个类
+            boolean cancel(boolean mayInterruptIfRunning) 试图取消对此任务的执行。
+            V get() 如有必要，等待计算完成，然后获取其结果。
+            V get(long timeout, TimeUnit unit) 如有必要，最多等待为使计算完成所给定的时间之后，获取其结果（如果结果可用）。
+            boolean isCancelled() 如果在任务正常完成前将其取消，则返回 true。
+            boolean isDone() 如果任务已完成，则返回 true。
+
          */
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         Future<Integer> submit = executorService.submit(new SumMethod());
