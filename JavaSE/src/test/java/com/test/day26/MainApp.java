040d7d5223a12d50c12db1c552326cf84b17a974
Memo <2415782224@qq.com>
2021-07-27 21:32:34 +0800

    uncomplete

---
+++ JavaSE/src/test/java/com/test/day26/MainApp.java
@@ -0,0 +1,16 @@
+package com.test.day26;
+
+import jdk.internal.util.xml.SAXParser;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.xml.sax.SAXException;
+
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws DocumentException, SAXException {
+
+    }
+}
---
+++ JavaSE/src/test/java/com/test/day26/Test.java
@@ -0,0 +1,10 @@
+package com.test.day26;
+
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        long time = new Date().getTime();
+        
+    }
+}
