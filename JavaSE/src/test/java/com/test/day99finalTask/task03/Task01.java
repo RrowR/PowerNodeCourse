19e3d5bde067db80f095a032ad41b998a21b2ce5
Memo <2415782224@qq.com>
2021-08-02 19:18:23 +0800

    多个线程模拟争抢（修正版）

--- JavaSE/src/test/java/com/test/day99finalTask/task03/Task01.java
+++ JavaSE/src/test/java/com/test/day99finalTask/task03/Task01.java
@@ -4,16 +4,27 @@ package com.test.day99finalTask.task03;
  * 编写多线程程序，模拟多个人通过一个山洞的模拟。这个山洞每次只能通过一个人，每个人通过山洞的时间为5秒，有10个人同时准备过此山洞，显示每次通过山洞人的姓名和顺序。
  */
 public class Task01 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            new Thread(() -> {
-                System.out.println(Thread.currentThread().getName() + "过去了");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    static Cave cave = new Cave();
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i < 10; i++) {
+            new Thread(() -> cave.passCave(),"线程" + i).start();
+        }
+    }
+}
+
+class Cave{
+    void passCave(){
+        // 每个线程都会来调用这个方法，但是Cave只有一个，所以可以用这个对象当作锁
+        synchronized (this){
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    System.out.print(Thread.currentThread().getName() + "走啊走走啊走...走了");
+                    System.out.println(i + "秒");
+                    Thread.sleep(1000);
                 }
-            }).start();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
