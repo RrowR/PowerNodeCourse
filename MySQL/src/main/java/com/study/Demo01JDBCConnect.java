d5bc68334ff9bc3d82768a48743665cd4abbb9a8
MeroMemo <1043526080@qq.com>
2021-08-10 17:51:46 +0800

    MYysqlbegin

--- MySQL/src/main/java/com/study/Demo01JDBCConnect.java
+++ MySQL/src/main/java/com/study/Demo01JDBCConnect.java
@@ -7,12 +7,12 @@ import java.sql.Statement;
 
 public class Demo01JDBCConnect {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        final String JDBC_URL="jdbc:mysql://127.0.0.1:3306/course?useUnicode=true&useSSL=false&characterEncoding=UTF8";
+        final String JDBC_URL="jdbc:mysql://127.0.0.1:3306/course?useUnicode=true&useSSL=false&characterEncoding=UTF8&serverTimezone=UTC";
         // 加载驱动
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(JDBC_URL, "root", "123456");
+        Connection connection = DriverManager.getConnection(JDBC_URL,"root","123456");
         Statement statement = connection.createStatement();
-        String sql = "INSERT INTO student(name,address,age,sex,birth) VALUES('小明','武汉',22,'男','2020-1-1')";
+        String sql="INSERT INTO student(name,address,age,sex,birth) VALUES('小明','武汉',22,'男','2020-1-1')";
         int i = statement.executeUpdate(sql);
         if ( i > 0){
             System.out.println("添加成功");
