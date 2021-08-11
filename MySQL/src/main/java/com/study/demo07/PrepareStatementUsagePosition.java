21d2d2c769e9e227d084d2b6f34f755bdbd1a814
Memo <2415782224@qq.com>
2021-08-12 02:18:28 +0800

    返回值有一点bug，困了不敲了 boolean hasResult = preparedStatement.execute();

--- MySQL/src/main/java/com/study/demo07/PrepareStatementUsagePosition.java
+++ MySQL/src/main/java/com/study/demo07/PrepareStatementUsagePosition.java
@@ -1,24 +1,34 @@
-package com.study.demo06;
+package com.study.demo07;
 
 import com.study.utils.DBUtils;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
-public class Demo01SQLInjectProblem {
+/**
+ * 在demo06中我们看到了sql注入的问题，那么解决办法如下
+ * 使用PrepareStatement可以解决SQL注入问题（可以使用占位符）
+ * 但是需要使用预处理PrepareStatement对象
+ */
+public class PrepareStatementUsagePosition {
     public static void main(String[] args) throws SQLException {
         Connection connection = DBUtils.getConnection();
-        Statement statement = connection.createStatement();
+        // 先创建一条带占位符的sql
+        String sql = "select * from sys_user where username = ? and password = ?";
+        // 使用connection中的prepareStatement方法来预编译这条sql
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入用户名");
         String username = scanner.next().trim();
+        // 这里是从1开始
+        preparedStatement.setString(1,username);
         System.out.println("请输入密码");
         String password = scanner.next().trim();
+        preparedStatement.setString(2,password);
         // 此查询判断的是返回是否是一个ResultSet，如果是返回true如果不是返回false
-        ResultSet hasResult = statement.executeQuery("select * from sys_user where username = '"+username+"' and password = '"+password+"'");
+        boolean hasResult = preparedStatement.execute();
         /*
             这种写法存在一个非常严重的sql注入问题
             当用户密码输入成以下格式：
@@ -28,7 +38,7 @@ public class Demo01SQLInjectProblem {
             select * from sys_user where username = "aaa" and password = "aaa'or'1'='1";
             在sql中返回是一个空值，但是确实有结果那么结果将会返回true
          */
-        if (hasResult.next()){
+        if (hasResult){
             System.out.println("登陆成功");
         }else {
             System.out.println("登陆失败");
