ebe0fa192c4a15884fe3853b26b0cadd69edecac
Memo <2415782224@qq.com>
2021-08-29 12:12:25 +0800

    注意:在调用 对象.属性 的时候，必须要有get方法，否则EL表达式取不出对象的属性

--- 04_Jsp/03_Servlet/src/main/java/com/domain/Student.java
+++ 04_Jsp/03_Servlet/src/main/java/com/domain/Student.java
@@ -1,16 +1,43 @@
 package com.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Student {
     private String name;
     private Integer age;
     private Date birth;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age, Date birth) {
+        this.name = name;
+        this.age = age;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
 }
--- 04_Jsp/03_Servlet/web/ELExpr3.jsp
+++ 04_Jsp/03_Servlet/web/ELExpr3.jsp
@@ -15,13 +15,20 @@
     <%--  使用原生jsp来存值  --%>
     <%
         request.setAttribute("user",new Student("猫羽雫",16,new Date()));
+        request.setAttribute("b",true);
     %>
     <%-- 使用EL来取值   --%>
+    <%--
+        这里还有一个问题，我对象的属性是私有的，但是还能直接获取对象的属性，看来是默认帮我们调用了get方法
+        如果没有get方法，那么这个对象.属性是点不出来的,服务器还会报500的错误
+   --%>
     ${user.name}<br/>
     ${user.age}<br/>
     <%-- 国际化时间   --%>
     ${user.birth}<br/>
     <%--  当前系统格式时间  --%>
     ${user.birth.toLocaleString()}<br/>
+    ${b};<br>
+
 </body>
 </html>
