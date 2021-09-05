09950dfd2afc37071fd81183241fc200cb48a5c4
MeroMemo <1043526080@qq.com>
2021-09-05 17:13:21 +0800

    解决sql查询问题，主要是因为配置文件里的数据库没有改对

--- 04_Jsp/51_CRM/src/main/java/dao/impl/UserDaoImpl.java
+++ 04_Jsp/51_CRM/src/main/java/dao/impl/UserDaoImpl.java
@@ -8,8 +8,7 @@ import domain.User;
 public class UserDaoImpl extends BaseDao implements UserDao {
     @Override
     public User login(String username, String password) {
-        String sql="SELECT id,username,password,realname as realName,role,deleted,img,create_time as createTime," +
-                "update_time as updateTime,deleted_time as deletedTime from user where username=? and password=?";
+        String sql="select id,username,password,realname as realName,role,deleted,img,create_time as createTime,update_time as updateTime,deleted_time as deletedTime from user where username = ? and password = ?";
         Object [] params={username,password};
         return super.selectOne(sql,User.class,params);
     }
