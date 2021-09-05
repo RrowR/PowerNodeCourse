ac0a29f505cffe4e41196757223a6106a78c00dd
MeroMemo <1043526080@qq.com>
2021-09-06 01:30:38 +0800

    XMLHttpRequest来作为ajax的异步请求处理

--- 04_Jsp/07_AJAX/src/main/java/com/study/CheckServlet.java
+++ 04_Jsp/07_AJAX/src/main/java/com/study/CheckServlet.java
@@ -7,11 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
-@WebServlet("/Check.do")
+@WebServlet("/check.do")
 public class CheckServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        ArrayList<String> names = new ArrayList<>();
+        names.add("zhangsan");
+        names.add("lisi");
+        names.add("wangwu");
+        String username = req.getParameter("username");
+        PrintWriter writer = resp.getWriter();
+        if (names.contains(username)){
+            writer.write("false");
+        }else {
+            writer.write("true");
+        }
     }
 }
