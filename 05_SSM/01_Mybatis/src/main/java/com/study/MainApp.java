c0702a4fece08b56bb8ee985446d76bd7b552a3b
Memo <2415782224@qq.com>
2021-09-07 15:48:11 +0800

    mybatis的第一个项目

---
+++ 05_SSM/01_Mybatis/src/main/java/com/study/MainApp.java
@@ -0,0 +1,7 @@
+package com.study;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 05_SSM/01_Mybatis/src/main/java/com/study/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.study.mapper;
+
+import com.study.domain.User;
+
+import java.util.Map;
+
+public interface UserMapper {
+
+    Map<String,Object> queryById(Integer id);
+
+    User queryById(String id);
+}
