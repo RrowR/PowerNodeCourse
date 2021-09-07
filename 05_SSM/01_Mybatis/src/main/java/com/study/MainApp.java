4a225abbe3fb4d07f938fcc4c9d91f78f1f49cf3
Memo <2415782224@qq.com>
2021-09-07 16:00:20 +0800

    使用mybatis查询出了数据库的一条数据

--- 05_SSM/01_Mybatis/src/main/java/com/study/MainApp.java
+++ 05_SSM/01_Mybatis/src/main/java/com/study/MainApp.java
@@ -1,7 +1,25 @@
 package com.study;
 
-public class MainApp {
-    public static void main(String[] args) {
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+        // 引入配置文件
+        String config = "mybatis.xml";
+        InputStream is = Resources.getResourceAsStream(config);
+        // 解析配置文件，构建SqlSessionFactory对象
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        // 创建一个具体的SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 进行数据库具体会话
+        Object o = sqlSession.selectOne("com.study.mapper.UserMapper.queryById1", 1);
+        System.out.println(o);
     }
 }
