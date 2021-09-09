da1b2261bb0c551f5ccf29d45ee5011a8a0a3f4c
Memo <2415782224@qq.com>
2021-09-09 18:58:35 +0800

    使用RowBounds完成分页查询，修改log配置文件

--- 05_SSM/01_Mybatis_04/src/test/java/com/test/TestPagingSearch.java
+++ 05_SSM/01_Mybatis_04/src/test/java/com/test/TestPagingSearch.java
@@ -17,7 +17,10 @@ public class TestPagingSearch {
     void PagingSearch01(){
         // 调用分页查询的类，Mybatis自带
         RowBounds rowBounds = new RowBounds(0,2);
-        // 调用mapper里的分页查询，传入RowBounds，获得分页查询对象，这里面的方法也是接口映射过来的
+        /*
+            调用mapper里的分页查询，传入RowBounds，获得分页查询对象，这里面的方法也是接口映射过来的
+            使用rowBounds来进行分页的查询首会查询所有，再帮你分页(分页不是在sql内部进行)，所以效率其实是很低的
+         */
         List<User> users = mapper.selectPage(rowBounds);
         users.forEach(System.out::println);
     }
