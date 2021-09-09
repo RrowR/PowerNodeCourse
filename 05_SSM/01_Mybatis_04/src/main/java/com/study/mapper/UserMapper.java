a66696a8a866f52d1f88e9687ccd0d96f867b596
Memo <2415782224@qq.com>
2021-09-09 10:29:40 +0800

    模糊查询的其他两种方式

--- 05_SSM/01_Mybatis_04/src/main/java/com/study/mapper/UserMapper.java
+++ 05_SSM/01_Mybatis_04/src/main/java/com/study/mapper/UserMapper.java
@@ -9,4 +9,14 @@ public interface UserMapper {
         模糊查询
      */
     public User selectByName(String name);
+
+    /*
+        模糊查询2
+     */
+    public User selectByName2(String name);
+
+    /*
+        模糊查询3
+     */
+    public User selectByName3(String name);
 }
--- 05_SSM/01_Mybatis_04/src/test/java/com/test/TestMethod.java
+++ 05_SSM/01_Mybatis_04/src/test/java/com/test/TestMethod.java
@@ -7,15 +7,28 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
 public class TestMethod {
+    SqlSession sqlSession = SqlSessionUtils.getsqlSession();
+    UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 
 
     @Test
     void Test01(){
-        SqlSession sqlSession = SqlSessionUtils.getsqlSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         User user = mapper.selectByName("猫");
         System.out.println("user = " + user);
+        sqlSession.close();
     }
 
+    @Test
+    void Test02(){
+        User user = mapper.selectByName2("猫");
+        System.out.println("user = " + user);
+        sqlSession.close();
+    }
 
+    @Test
+    void Test03(){
+        User user = mapper.selectByName3("猫");
+        System.out.println("user = " + user);
+        sqlSession.close();
+    }
 }
