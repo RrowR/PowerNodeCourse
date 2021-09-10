514b8f51bef71d6c4aae478665f2ed5d08e286b0
Memo <2415782224@qq.com>
2021-09-10 14:59:33 +0800

    ClassPathXmlApplicationContext 里的getBean方法来获得对象

--- 05_SSM/02_Spring/src/test/java/com/test/UserTest.java
+++ 05_SSM/02_Spring/src/test/java/com/test/UserTest.java
@@ -8,8 +8,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class UserTest {
     @Test
     void TestBean(){
+        // 加载配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        Object user = context.getBean("user");
+        // 从容器中获取对象,这里使用类名还是使用id都是可以的
+        Object user = context.getBean("userId");
         System.out.println(user);
     }
 }
