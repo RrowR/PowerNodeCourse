86ae41dc6f1a271408fd728bc3db327570999381
Memo <2415782224@qq.com>
2021-09-11 17:31:11 +0800

    静态工厂还没写完

--- 05_SSM/02_Spring/src/test/java/com/test/UserTest.java
+++ 05_SSM/02_Spring/src/test/java/com/test/UserTest.java
@@ -19,11 +19,13 @@ public class UserTest {
     @Test
     void TestBean2(){
         /*
-            当获取容器为字节码的时候
+            当获取容器为字节码的时候,如果其中有2个对象的字节码是同一个对象，那么Spring将无法区分到底使用哪个字节码
+            我们需要对其中一个字节码添加primary属性，才可以生效，而不能达到3个，否则一个有primary另一个又不知道了
          */
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         Object user = context.getBean(Student.class);
-
     }
+
+
 
 }
