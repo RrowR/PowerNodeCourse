e0127bfcebe276ae081737708f154ba2eb038c18
MeroMemo <1043526080@qq.com>
2021-09-12 23:36:34 +0800

    使用cglib的动态代理

---
+++ 05_SSM/02_Spring_Proxy02_Cglib/src/test/java/com/test/TestCglib.java
@@ -0,0 +1,18 @@
+package com.test;
+import com.study.MyMethodInterceptor;
+import com.study.TargetClass;
+import net.sf.cglib.proxy.Enhancer;
+
+public class TestCglib {
+    public static void main(String[] args) {
+        // 增强工具类，等下可以创建代理对象
+        Enhancer enhancer = new Enhancer();
+        // 给增强类设置一个爸爸
+        enhancer.setSuperclass(TargetClass.class);
+        // 给增强类设置调用时的回调
+        enhancer.setCallback(new MyMethodInterceptor());
+        // 创建父类代理对象
+        TargetClass proxy = (TargetClass) enhancer.create();
+        proxy.rent();
+    }
+}
