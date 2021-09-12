1926d085a8900ccd1c6c36fdf6c29f309b347c71
Memo <2415782224@qq.com>
2021-09-12 19:15:33 +0800

    使用set方法的时候这个类必须要有set方法，否则无法在进行注入（在property里也不会有参数提示） 注意:使用set进行注入的时候，一定要有无参构造方法，和有参构造没有关系

--- 05_SSM/02_Spring/src/test/java/com/test/TestStudentInject.java
+++ 05_SSM/02_Spring/src/test/java/com/test/TestStudentInject.java
@@ -1,6 +1,7 @@
 package com.test;
 
 import com.study.domain.TestInjectUser01;
+import com.study.domain.TestInjectUser02;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,5 +18,18 @@ public class TestStudentInject {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         TestInjectUser01 bean = context.getBean(TestInjectUser01.class);
         System.out.println(bean);
+        // 如果这里使用id来获取，那么spring不知道这个是什么对象，只能使用强转才可以，但是我们作为上帝视角是知道的
+        Object bean2 = context.getBean("testConstructorInject1_2");
+        System.out.println("bean2 = " + bean2);
+    }
+
+    @Test
+    void Test02(){
+        /*
+            使用Set方法进行注入
+         */
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        TestInjectUser02 bean = context.getBean(TestInjectUser02.class);
+        System.out.println("bean = " + bean);
     }
 }
