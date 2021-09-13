8c8cba5f4b6d1faa04805f1a7ddf25e2effa17ea
Memo <2415782224@qq.com>
2021-09-13 13:01:11 +0800

    使用实现Controller接口的方式返回一个ModelAndView,通过设置setViewName 来设置请求的地址(默认是转发)

--- 05_SSM/04_SpringMVC/src/main/java/com/study/controller/Controller02.java
+++ 05_SSM/04_SpringMVC/src/main/java/com/study/controller/Controller02.java
@@ -12,8 +12,9 @@ public class Controller02 implements Controller {
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         System.out.println("springMVC 02");
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("name","Hello MVC02");
-        modelAndView.setViewName("/index.jsp");
+        // ModelAndView 内部使用的是内部转发的方式
+//        modelAndView.setViewName("/index.jsp");
+        modelAndView.setViewName("redirect:index.jsp");     // 手动加可以改请求方式
         return modelAndView;
     }
 }
