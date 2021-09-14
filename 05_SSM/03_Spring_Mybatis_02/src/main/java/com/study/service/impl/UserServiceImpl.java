89b707e0920ee801c5943fbd5824cedabfa868dd
Memo <2415782224@qq.com>
2021-09-14 16:37:09 +0800

    使用纯spring配置文件的方式来管理mybatis文件

---
+++ 05_SSM/03_Spring_Mybatis_02/src/main/java/com/study/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.study.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.study.domain.User;
+import com.study.mapper.UserMapper;
+import com.study.service.UserService;
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return userMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int insert(User record) {
+        return userMapper.insert(record);
+    }
+
+    @Override
+    public int insertSelective(User record) {
+        return userMapper.insertSelective(record);
+    }
+
+    @Override
+    public User selectByPrimaryKey(Integer id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(User record) {
+        return userMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(User record) {
+        return userMapper.updateByPrimaryKey(record);
+    }
+
+}
