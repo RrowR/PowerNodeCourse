fe873da3d3970526e19998b3561c97865ad39945
MeroMemo <1043526080@qq.com>
2021-09-21 16:42:21 +0800

    记录登陆日志，修改显示日志的时间为倒序

--- 05_SSM/90_CarProject/src/main/java/com/study/system/controller/LoginController.java
+++ 05_SSM/90_CarProject/src/main/java/com/study/system/controller/LoginController.java
@@ -3,14 +3,16 @@ package com.study.system.controller;
 import cn.hutool.crypto.digest.DigestUtil;
 import com.study.system.common.Constants;
 import com.study.system.common.Result;
+import com.study.system.domain.LogLogin;
 import com.study.system.domain.User;
+import com.study.system.service.LogLoginService;
 import com.study.system.service.UserService;
 import com.study.system.utils.WebUtils;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /*
     这里需要使用RestController来将对象转换成json对象，前端才能接收，不然前端会认为是一个Object，那边无法获得msg
@@ -20,6 +22,9 @@ import javax.annotation.Resource;
 public class LoginController {
     @Resource
     private UserService userService;
+
+    @Resource
+    private LogLoginService logLoginService;
 
     /*
         跳转到登录界面
@@ -32,6 +37,8 @@ public class LoginController {
         if (captcha.equalsIgnoreCase(WebUtils.getSession().getAttribute(Constants.CHECK_CODE_KEY).toString())){
             if (user != null){
                 WebUtils.getSession().setAttribute(Constants.CURRENT_SESSION_USER_KEY,user);
+                // 在登陆成功的时候记录日志
+                logLoginService.insert(new LogLogin(user.getUserid(),user.getRealname(),user.getAddress(),new Date()));
                 return new Result(200,"登录成功");
             }else {
                 return new Result(-1,"用户名或密码不正确");
--- 05_SSM/90_CarProject/src/main/java/com/study/system/domain/LogLogin.java
+++ 05_SSM/90_CarProject/src/main/java/com/study/system/domain/LogLogin.java
@@ -16,6 +16,8 @@ public class LogLogin {
     private String loginname;
 
     private String loginip;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" , timezone = "GMT+8")
     private Date logintime;
+
+
 }
