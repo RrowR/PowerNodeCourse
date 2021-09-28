f71db39b6b5ed776cabe445dd069a8f006f010fd
Memo <2415782224@qq.com>
2021-09-28 15:50:07 +0800

    使用yml文件来进行注入属性

---
+++ 06_SpringBoot/02_SpringBoot_DI/src/main/java/com/study/bean/User.java
@@ -0,0 +1,13 @@
+package com.study.bean;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "user")
+public class User {
+    private String name;
+    private Integer age;
+}
