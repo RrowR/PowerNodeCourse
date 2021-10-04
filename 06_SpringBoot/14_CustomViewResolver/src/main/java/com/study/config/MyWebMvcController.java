83f6008d1227cd193e160796d3ed3489b8af3f83
Memo <2415782224@qq.com>
2021-10-04 12:39:27 +0800

    自定义视图解析器(重定向和转发)，注意要引入thymeleaf模板引擎

--- 06_SpringBoot/14_CustomViewResolver/src/main/java/com/study/config/MyWebMvcController.java
+++ 06_SpringBoot/14_CustomViewResolver/src/main/java/com/study/config/MyWebMvcController.java
@@ -1,9 +1,16 @@
 package com.study.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyWebMvcController implements WebMvcConfigurer {
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 重定向控制器注册
+        registry.addRedirectViewController("/tobaidu","https://www.bilibili.com/");
+        // 注册转发控制器
+        registry.addViewController("/test").setViewName("test");
+    }
 }
