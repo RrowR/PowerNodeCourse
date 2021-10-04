6f064ef8ab0f22728b80c9ef5903483a1154a296
Memo <2415782224@qq.com>
2021-10-04 14:04:15 +0800

    注册自定义拦截器

---
+++ 06_SpringBoot/15_CustomInterceptor/src/main/java/com/study/config/MyInterceptor.java
@@ -0,0 +1,18 @@
+package com.study.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("进来了");
+        return false;       // true为放行，false为拦截
+    }
+}
