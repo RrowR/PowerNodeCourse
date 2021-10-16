89bd466fa8ddc32bd4dd8bbc2027edc3963cd365
Memo <2415782224@qq.com>
2021-10-16 16:39:10 +0800

    当没有权限访问的时候，可以在静态资源文件里创建一个static/error/403.html文件，当出现这个异常的时候自动跳转到里面去

---
+++ 09_SpringSecurity/02_Annotation/src/main/java/com/study/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("hello")
+    public String getMsg(){
+        return "hello security";
+    }
+}
