2fe8c6dc290b04cf2d1843cab9ac546b90bf1283
Memo <2415782224@qq.com>
2021-10-16 10:38:40 +0800

    获取当前登录用户的信息

---
+++ 09_SpringSecurity/01_Security_Hello/src/main/java/com/study/controller/GerUserInfoController.java
@@ -0,0 +1,28 @@
+package com.study.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/*
+    获取当前用户信息的2种方式
+ */
+@RestController
+public class GerUserInfoController {
+    @RequestMapping("userinfo")
+    public Principal getUserInfo(Principal principal){
+        // 从 Principal 对象中进行获取
+        return principal;
+    }
+
+    @RequestMapping("userinfo2")
+    public Principal getUserInfo2(){
+        // 获取SecurityContextHolder 上下文的方式
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication;
+    }
+
+}
