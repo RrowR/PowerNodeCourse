460efe4d07281101d47cd7cb3d09a9989fb3ddf3
Memo <2415782224@qq.com>
2021-10-16 09:49:33 +0800

    security的使用

---
+++ 09_SpringSecurity/01_Security_Hello/src/main/java/com/study/config/MyWebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hxd")
+                .password(passwordEncoder().encode("hxd"))          // 这里只能加密不能解密，然后进行输入验证
+                .roles("ADMIN")
+                .and()
+                .withUser("hxh")
+                .password(passwordEncoder().encode("hxh"))
+                .roles("TEST");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
