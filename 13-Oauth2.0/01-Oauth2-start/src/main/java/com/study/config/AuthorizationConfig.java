25429e7982a928028c6d90cce6233a32beea279f
Memo <2415782224@qq.com>
2021-10-30 10:58:21 +0800

    Oauth2.0在授权之前需要有一个用户先来登录才可以进行授权

---
+++ 13-Oauth2.0/01-Oauth2-start/src/main/java/com/study/config/AuthorizationConfig.java
@@ -0,0 +1,49 @@
+package com.study.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+@Configuration
+public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("wx")
+                .secret(passwordEncoder().encode("wx-secret"))      // 使用方法来加密不需要注入
+                .scopes("read")
+                .authorizedGrantTypes("authorization_code")
+                .accessTokenValiditySeconds(7200)
+                .redirectUris("https://www.baidu.com");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 使用redis做token存储
+        endpoints.tokenStore(tokenStore());
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ 13-Oauth2.0/01-Oauth2-start/src/main/java/com/study/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.study.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .authorities("sys:add");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
