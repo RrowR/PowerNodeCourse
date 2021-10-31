732d20159c189996c40c8eb0a806f4bec95c3b05
MeroMemo <1043526080@qq.com>
2021-10-31 14:20:31 +0800

    断言工厂的使用

--- 12-spring-cloud/05-gateway/01-gateway/src/main/java/com/study/config/MyRouter.java
+++ 12-spring-cloud/05-gateway/01-gateway/src/main/java/com/study/config/MyRouter.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.ZonedDateTime;
+
 /**
  *  使用java代码来配置路由
  */
@@ -16,5 +18,14 @@ public class MyRouter {
         return builder.routes()
                 .route("222", r -> r.path("/").uri("http://www.baidu.com"))
                 .build();
+    }
+
+    /*
+        断言表达式地址(spring官方文档)
+        https://docs.spring.io/spring-cloud-gateway/docs/2.2.5.RELEASE/reference/html/#gateway-request-predicates-factories
+     */
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
     }
 }
